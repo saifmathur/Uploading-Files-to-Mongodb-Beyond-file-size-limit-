080950209fc2d34a3b7b8efd4a826b80b3343d28
saifmathur <saifmathur41@gmail.com>
2020-06-13 16:55:46 +0530

    module for reading image file from the database

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post('/upload',upload.single('file'),function (req, res){   //'file' comes f
 app.get('/files', function(req,res){
     //find file
     gfs.files.find().toArray(function (err, files){
-        if (!files || files.lengh === 0){
+        if (!files || files.length === 0){
             return res.status(404).json({
                 err: 'No file found'
             })
@@ -78,6 +78,47 @@ app.get('/files', function(req,res){
 
     })
 })
+
+
+//to get files by file name in the url
+app.get('/files/:filename', function(req,res){
+    //find file
+    gfs.files.findOne({filename: req.params.filename}, function(err, file){
+        if (!file || file.length === 0){
+            return res.status(404).json({
+                err: 'No file found'
+            })
+        }
+        //file exists
+        return res.json(file)
+    })
+})
+
+//now to add a route specifically for images, so that we can read and output the image
+app.get('/image/:filename', function(req,res){
+    //find file
+    gfs.files.findOne({filename: req.params.filename}, function(err, file){
+        if (!file || file.length === 0){
+            return res.status(404).json({
+                err: 'No file found'
+            })
+        }
+        //check if its an image or not
+        if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
+            //read output to browser
+            const readstream = gfs.createReadStream(file.filename)
+            readstream.pipe(res)
+
+        }
+        else {
+            res.status(404).json({
+                err: 'Not an image'
+            })
+        }
+    })
+})
+
+
 
 
 
